ab0f79f01daf1ebf64a179312ab3ab6acd2e3265
Rajdeep <rajdeep.sarkar@insight-centre.org>
2020-12-05 15:39:33 +0000

    DeepANT code added

--- rajdeep/codes/dataset.py
+++ rajdeep/codes/dataset.py
@@ -7,14 +7,14 @@ from utils import read_folder_files_regression, read_folder_files_prediction
 class HuaweiRegressionDataset(Dataset):
     def __init__(self, opt):
         self.train_folder_path = opt["training_data_folder"]
-        self.dev_folder_path = opt["dev_data_folder"]
+        # self.dev_folder_path = opt["dev_data_folder"]
         self.n_steps = opt["n_steps"]
         self.transform = False
 
         self.train_data = read_folder_files_regression(self.train_folder_path, self.n_steps)
-        self.dev_data = read_folder_files_regression(self.dev_folder_path, self.n_steps)
-        self.data = self.train_data + self.dev_data
-
+        # self.dev_data = read_folder_files_regression(self.dev_folder_path, self.n_steps)
+        # self.data = self.train_data + self.dev_data
+        self.data = self.train_data
     def __len__(self):
         return len(self.data)
     
